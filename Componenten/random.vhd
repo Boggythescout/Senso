387e86dcf74944c42165bd8607d936708acf3937
Boggy <lukasbockstaller@gmail.com>
2016-04-26 14:12:32 +0200

    Random fertig

--- Componenten/random.vhd
+++ Componenten/random.vhd
@@ -19,7 +19,7 @@
 ----------------------------------------------------------------------------------
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-
+use work.all;
 -- Uncomment the following library declaration if using
 -- arithmetic functions with Signed or Unsigned values
 --use IEEE.NUMERIC_STD.ALL;
@@ -37,9 +37,40 @@ Port(
 end random;
 
 architecture Behav of random is
-
+	Signal q_in, q: std_logic_vector(7 downto 0);
 begin
 
+	zufall: entity random_gen
+	PORT Map(
+		q_in=>q_in,
+		q=>q
+	);
 
+	process(clk, res_n)
+		variable SR, MR: std_logic_vector(7 downto 0);
+	begin
+		if res_n='0' then
+			SR:=B"0000_0001";
+			MR:=(others => '0');
+		else
+			if clk='1' and clk'event then
+				if restore='1' then
+					SR:=MR;
+				else
+					if shift='1' then
+						q_in<=SR;
+						SR:=q;
+					end if;
+				end if ;
+
+				if store='1' then
+					MR:=SR;
+				end if;
+
+				rnd<=SR(1 downto 0);
+
+			end if;
+		end if;
+	end process;
 end Behav;
 
